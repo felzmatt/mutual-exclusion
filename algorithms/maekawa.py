9987b79db383b7cc5b4dd4f2c03e4415a5fab941
felzmatt <felmat23@gmail.com>
2023-06-30 20:08:09 +0200

    implement maekawa algorithm

--- algorithms/maekawa.py
+++ algorithms/maekawa.py
@@ -53,6 +53,9 @@ def get_voting_set(procID: int, peers: List[int]) -> Set[int]:
     # print(matrix)
     return voting
 
+def select_next(pending: List[int]) -> int:
+    return pending.pop(0)
+
 
 REQUEST = 77
 REPLY = 88
@@ -64,7 +67,7 @@ def maekawa(cs_time: int, my_id: int, peers: List[int], router_sock) -> None:
     state = State.RELEASED
     voted = False
     V = get_voting_set(my_id, peers)
-    replies = []
+    replies = 0
     pending = []
     
     enters = []
@@ -75,18 +78,18 @@ def maekawa(cs_time: int, my_id: int, peers: List[int], router_sock) -> None:
     # event loopS
     while True:
         # print("looping")
-        if state == State.REQUESTING:
-            if replies == len(peers):
+        if state == State.WANTED:
+            if replies == len(V) - 1:
                 # received all REPLY required to access CS
                 # access cs
                 # TODO: make access call
                 print("I AM ENTERING CS")
                 r = requests.post("http://cs:5000/enter_cs",data={"procID":my_id})
                 if r.status_code == 200:
-                    state = State.CS
+                    state = State.HELD
                     enters.append(time.time())
         
-        elif state == State.CS:
+        elif state == State.HELD:
             now = time.time()
             print(now - enters[-1])
             if now - enters[-1] > cs_time:
@@ -96,12 +99,14 @@ def maekawa(cs_time: int, my_id: int, peers: List[int], router_sock) -> None:
                 print("I AM LEAVING CS")
                 r = requests.post("http://cs:5000/leave_cs",data={"procID":my_id})
                 # send replies
-                for req in Q:
-                    reply = create_message(sender=my_id, receiver=req[1], msg_type=REPLY, ts=num)
-                    router_sock.sendall(reply)
-                Q.clear()
-                state = State.NCS
+                state = State.RELEASED
                 replies = 0
+                for peer in V:
+                    if peer != my_id:
+                        rel = create_message(sender=my_id, receiver=peer, msg_type=MSGType.REL, ts=0)
+                        router_sock.sendall(rel)
+
+
         
         # readable, _, _ = select.select([router_sock], [], [])
         # for sock in readable:
@@ -114,26 +119,35 @@ def maekawa(cs_time: int, my_id: int, peers: List[int], router_sock) -> None:
                 print(f"Received {format_message(sender=sender, receiver=receiver, msg_type=msg_type, ts=ts)}")
         
             # upon receipt of REQUEST
-            if msg_type == REQUEST:
-                if state == State.CS or (state == State.REQUESTING and (last_req, my_id) < (ts, sender)):
-                    Q.append((ts, sender))
+            if msg_type == MSGType.REQ:
+                if state == State.HELD or voted:
+                    pending.append(sender)
                 else:
-                    reply = create_message(sender=my_id, receiver=sender, msg_type=REPLY, ts=0)
-                    router_sock.sendall(reply)
-                num = max(ts, num)
-        
-            # upon receipt of REPLY
-            elif msg_type == REPLY:
+                    ack = create_message(sender=my_id, receiver=sender, msg_type=MSGType.ACK, ts=0)
+                    router_sock.sendall(ack)
+                    voted = True
+            
+            # upon receipt of ACK
+            elif msg_type == MSGType.ACK:
                 replies += 1
+        
+            # upon receipt of RELEASE
+            elif msg_type == MSGType.REL:
+                if len(pending) > 0:
+                    candidate = select_next(pending)
+                    # pending.remove(candidate)
+                    ack = create_message(sender=my_id, receiver=candidate, msg_type=MSGType.ACK, ts=0)
+                    router_sock.sendall(ack)
+                    voted = True
+                else:
+                    voted = False
 
             # upon receive ENTER_CS
             elif msg_type == ENTER_CS:
-                if state == State.NCS:
-                    state = State.REQUESTING
-                    num += 1
-                    last_req = num
-                    for peer in peers:
-                        req = create_message(sender=my_id, receiver=peer, msg_type=REQUEST, ts=num)
+                state = State.WANTED
+                for peer in V:
+                    if peer != my_id:
+                        req = create_message(sender=my_id, receiver=peer, msg_type=MSGType.REQ, ts=0)
                         router_sock.sendall(req)
         except Exception as e:
             pass
